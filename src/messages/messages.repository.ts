0c320adf0e139a3bcba1a520594a90a19fe73406
VinayPanchal1974 <vinaypanchal115@gmail.com>
2025-01-07 13:57:31 +0530

    added the repository file for messages

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,18 +1,26 @@
-import {readFile, writeFile} from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises';
 
-export class MessagesRepository{
-    async findOne(id:string){
-        const contents = await readFile('message.json','utf-8');
-        const messages = JSON.parse(contents);
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
 
-        return messages[id];
-    }
+    return messages[id];
+  }
 
-    async findAll(){
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
 
-    }
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
 
-    async create(message:string){
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, content };
 
-    }
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
